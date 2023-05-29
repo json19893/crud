68e22503f76d9fc4a68f47e35c15256efe4c32ad
RicardoS76 <rdorica76@gmail.com>
2023-05-28 19:52:01 -0600

    ajustes

--- game_front/src/app/services/AppUrlSettings.ts
+++
@@ -1,9 +0,0 @@
-export class AppUrlSettings{
-    public static BASE_API = 'http://localhost:9081/game/';
-    public static GET_ALL_GAME= 'getAllGame'
-    public static PUT_GAME= 'putGame'
-    public static DELETE_GAME= 'deleteGame/'
-    public static EDIT_GAME= 'updateGame'
-    public static GETBYID_GAME= 'getByidGame/'
-
-}
